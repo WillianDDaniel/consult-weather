fb0c521548bc3d66e319767ea4c18227186affb7
Willian Daniel <williandeivitidaniel@live.com>
2025-04-27 00:21:07 -0300

    remove unused files and restructure project assets

---
+++ assets/scripts.js
@@ -0,0 +1,9 @@
+// const key = '42635e3cbf75489f4211bf361420053f'
+// const url = 'https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${key}&lang=${langcid}&units=metric'
+
+document.getElementById('toggle-menu-button').addEventListener('click', () => {
+  document.getElementById('line-top').classList.toggle('line-top-checked');
+  document.getElementById('line-middle').classList.toggle('line-middle-checked');
+  document.getElementById('line-bottom').classList.toggle('line-bottom-checked');
+  document.getElementById('menu').classList.toggle('menu-hidden');
+});
