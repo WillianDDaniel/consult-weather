ef89c5371e97d7656058fae301b92b6ecbc3bcce
Willian Daniel <williandeivitidaniel@live.com>
2025-04-29 09:00:56 -0300

    remove unused background image, add new weather icons and temperature icon, update scripts for weather fetching, enhance styles and layout in index.html

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,7 @@
 // const key = '42635e3cbf75489f4211bf361420053f'
 // const url = 'https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${key}&lang=${langcid}&units=metric'
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// https://openweathermap.org/img/wn/03d@2x.png
 
 document.getElementById('toggle-menu-button').addEventListener('click', () => {
   document.getElementById('line-top').classList.toggle('line-top-checked');
@@ -11,3 +13,36 @@ document.getElementById('toggle-menu-button').addEventListener('click', () => {
 document.getElementById('header-search-button').addEventListener('click', () => {
   document.getElementById('search-container').classList.toggle('search-container-hidden');
 });
+
+function getWeather() {
+  const key = '42635e3cbf75489f4211bf361420053f';
+  const cidade = 'Sao Paulo';
+  const langcid = 'pt_br';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=${langcid}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
+}
+
+function getForecast() {
+  const key = '42635e3cbf75489f4211bf361420053f';
+  const cidade = 'Sao Paulo';
+  const langcid = 'pt_br';
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${key}&lang=${langcid}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
+}
+
+function getWeatherIcon(iconCode) {
+  return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+}
+
+// getWeather();
+// getForecast();
